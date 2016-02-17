889cfbd1c990c8c42d9cf38ea8f9dd336af526f9
Josh La Plante <jlaplante13@ucsbalum.com>
2016-02-17 14:33:55 -0800

    updated number sorter

--- Python/Number Sorter/number_sorter.py
+++ Python/Number Sorter/number_sorter.py
@@ -1,4 +1,20 @@
-unsortedList = [67, 45, 2, 13, 1, 998]
-sortedList = sorted(unsortedList)
-print("before: \n",unsortedList)
-print("after: \n",sortedList)
+def easyNumberSorter():
+    unsortedList = [67, 45, 2, 13, 1, 998]
+    sortedList = sorted(unsortedList)
+    print("before: \n",unsortedList)
+    print("after: \n",sortedList)
+
+
+def myNumberSorter():
+    unsortedList = [67, 45, 2, 13, 1, 998]
+    workingList = [67, 45, 2, 13, 1, 998]
+    sortedList = []
+    while len(workingList) >0:
+        num = min(workingList)
+        sortedList.append(num)
+        workingList.remove(num)
+    print("before: \n",unsortedList)
+    print("after: \n",sortedList)
+
+
+myNumberSorter()
