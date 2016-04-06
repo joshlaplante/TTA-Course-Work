f407d96f9b168d53a3800f59ee1d7f12df9c3bea
Josh La Plante <jlaplante13@ucsbalum.com>
2016-04-06 11:35:47 -0700

    added c# directory and first projects

---
+++ C#/CSharp Program 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Program_1
+{
+    public class Program
+    {
+        //This is where the program starts
+        static void Main(string[] args)
+        {
+            //Prompt user to enter a name
+            Console.WriteLine("Enter your name:");
+
+            //Now read the name entered
+            string name = Console.ReadLine();
+
+            //Greet the user with entered name
+            Console.WriteLine("Hello, " + name);
+
+            //Wait for user to acknowledge result
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
---
+++ C#/TestApp/TestApp.cs
@@ -0,0 +1,14 @@
+// A simple C# application.
+using System;
+
+using System.Windows.Forms;
+
+class TestApp
+{
+	static void Main()
+	{
+		Console.WriteLine("Testing! 1,2,3");
+
+		MessageBox.Show("Hello...");
+	}
+}
